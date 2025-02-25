d95fb6d76a54cec1e43592a1c6747ed561e9499e
Maor Haimovitz <maor@mcsimw.com>
2025-02-25 14:52:11 -0500

    yes

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "My new flake with merged nixosModules";
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+    };
+    nix-genesis = {
+      type = "github";
+      owner = "mcsimw";
+      repo = "nix-genesis";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      type = "github";
+      owner = "hercules-ci";
+      repo = "flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, lib, ... }:
+      {
+        perSystem.treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+            dos2unix.enable = true;
+          };
+        };
+        imports = [
+          inputs.treefmt-nix.flakeModule
+        ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        flake.nixosModules = {
+          zfs-rollback = import ./nixosModules/zfs-rollback.nix;
+          zfsonnix = lib.modules.importApply ./nixosModules/zfs-rollback.nix {
+            localFlake = self;
+          };
+        };
+      }
+    );
+}
---
+++ nixosModules/zfsonix/default.nix
@@ -0,0 +1,50 @@
+{ localFlake, ... }:
+{ lib, config, ... }:
+let
+  cfg = config.zfsonix;
+in
+{
+  options.zfsonix = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the nixOnZfsos configuration.";
+    };
+    diskName = lib.mkOption {
+      type = lib.types.str;
+      default = "zfsos-disk";
+      description = "The name of the disk to be used.";
+    };
+    device = lib.mkOption {
+      type = lib.types.path;
+      default = "/dev/sdX";
+      description = "The block device path for the disk.";
+    };
+    ashift = lib.mkOption {
+      type = lib.types.int;
+      default = 12;
+      description = "The ashift value for ZFS (log₂ of the sector size).";
+    };
+    swapSize = lib.mkOption {
+      type = lib.types.str;
+      default = "2G";
+      description = "The size of the swap partition.";
+      check =
+        value:
+        let
+          m = builtins.match "^[1-9][0-9]*(M|G)$" value;
+        in
+        m != null;
+    };
+  };
+  config = lib.mkIf (cfg.enable && cfg.template == "zfsos") (
+    { }
+    // (
+      (import ./settings.nix {
+        inherit (cfg) diskName;
+        inherit localFlake;
+      })
+      (import ../../templates/zfsonix.nix { inherit (cfg) diskName device ashift; })
+    )
+  );
+}
