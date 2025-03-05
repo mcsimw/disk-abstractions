d0fabdd63a64a17c4389cc348583e6ff76dfea15
Maor Haimovitz <maor@mcsimw.com>
2025-03-05 17:43:50 -0500

    yes

--- nixosModules/zfsonix/settings.nix
+++ nixosModules/zfsonix/settings.nix
@@ -9,6 +9,11 @@
     "/persist".neededForBoot = true;
     "/mnt/${diskName}".neededForBoot = true;
   };
+  zfs-rollback = {
+    enable = true;
+    snapshot = "blank";
+    volume = "${diskName}-zfsos-zpool/faketmpfs";
+  };
   environment.persistence."/persist" = {
     enable = true;
     hideMounts = true;
