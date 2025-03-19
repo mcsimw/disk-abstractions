ca9445513f98e8f576a9da6eb07ae00993b41396
Maor Haimovitz <maor@mcsimw.com>
2025-03-19 09:59:31 -0400

    add preservation

--- nixosModules/zfsonix/settings.nix
+++ nixosModules/zfsonix/settings.nix
@@ -4,32 +4,46 @@
   options,
   ...
 }:
-{
-  boot = {
-    kernelParams = [ "nohibernate" ];
-    tmp.cleanOnBoot = true;
-  };
 
-  fileSystems = {
-    "/".neededForBoot = true;
-    "/persist".neededForBoot = true;
-    "/mnt/${diskName}".neededForBoot = true;
-  };
+lib.mkMerge [
+  {
+    boot = {
+      kernelParams = [ "nohibernate" ];
+      tmp.cleanOnBoot = true;
+    };
 
-  zfs-rollback = {
-    enable = true;
-    snapshot = "blank";
-    volume = "${diskName}-zfsos/faketmpfs";
-  };
-}
-// lib.optionalAttrs (options ? environment.persistence) {
-  environment.persistence."/persist" = {
-    enable = true;
-    hideMounts = true;
-    directories = [
-      "/var/lib/nixos"
-      "/var/log"
-      "/var/lib/systemd/coredump"
-    ];
-  };
-}
+    fileSystems = {
+      "/".neededForBoot = true;
+      "/persist".neededForBoot = true;
+      "/mnt/${diskName}".neededForBoot = true;
+    };
+
+    zfs-rollback = {
+      enable = true;
+      snapshot = "blank";
+      volume = "${diskName}-zfsos/faketmpfs";
+    };
+  }
+  (lib.optionalAttrs (options ? environment.persistence && !(options ? preservation)) {
+    environment.persistence."/persist" = {
+      enable = true;
+      hideMounts = true;
+      directories = [
+        "/var/lib/nixos"
+        "/var/log"
+        "/var/lib/systemd/coredump"
+      ];
+    };
+  })
+  (lib.optionalAttrs (options ? preservation) {
+    preservation = {
+      preserveAt."/persistent" = {
+        directories = [
+          "/var/lib/nixos"
+          "/var/log"
+          "/var/lib/systemd/coredump"
+        ];
+      };
+    };
+  })
+]
